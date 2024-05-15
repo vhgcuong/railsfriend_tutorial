1a49b9552a7c0afec0e7dc683b3e671f937b0d3a
Yotoko <vhgcuong95@gmail.com>
2024-05-15 22:10:29 +0700

    Init project

--- drkiq/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  #get 'pages/home'
-  root 'pages#home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
